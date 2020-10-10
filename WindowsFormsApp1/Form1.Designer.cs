36f142078b6b8f173f6f3a03ad51ecf2b202487a
M093040045@365mail.onmicrosoft.com <M093040045@365mail.onmicrosoft.com>
2020-10-10 16:22:54 +0800

    initial code

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel_Degree_value = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel_timer_interval = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Time_Start_checkBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(0, 341);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(434, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel_timer_interval,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel_Degree_value});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 389);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(434, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(55, 17);
+            this.toolStripStatusLabel1.Text = "Degree :";
+            // 
+            // toolStripStatusLabel_Degree_value
+            // 
+            this.toolStripStatusLabel_Degree_value.Name = "toolStripStatusLabel_Degree_value";
+            this.toolStripStatusLabel_Degree_value.Size = new System.Drawing.Size(14, 17);
+            this.toolStripStatusLabel_Degree_value.Text = "0";
+            // 
+            // toolStripStatusLabel_timer_interval
+            // 
+            this.toolStripStatusLabel_timer_interval.Name = "toolStripStatusLabel_timer_interval";
+            this.toolStripStatusLabel_timer_interval.Size = new System.Drawing.Size(14, 17);
+            this.toolStripStatusLabel_timer_interval.Text = "1";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(89, 17);
+            this.toolStripStatusLabel3.Text = "Timer Interval :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Time Interval TrackBar";
+            // 
+            // Time_Start_checkBox
+            // 
+            this.Time_Start_checkBox.AutoSize = true;
+            this.Time_Start_checkBox.Checked = true;
+            this.Time_Start_checkBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.Time_Start_checkBox.Location = new System.Drawing.Point(300, 321);
+            this.Time_Start_checkBox.Name = "Time_Start_checkBox";
+            this.Time_Start_checkBox.Size = new System.Drawing.Size(72, 16);
+            this.Time_Start_checkBox.TabIndex = 4;
+            this.Time_Start_checkBox.Text = "Time Start";
+            this.Time_Start_checkBox.UseVisualStyleBackColor = true;
+            this.Time_Start_checkBox.CheckedChanged += new System.EventHandler(this.Time_Start_checkBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(434, 411);
+            this.Controls.Add(this.Time_Start_checkBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_Degree_value;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_timer_interval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox Time_Start_checkBox;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        Pen blackPen = new Pen(Color.Black, 3);
+
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripStatusLabel_timer_interval.Text = trackBar1.Value + "ms";
+        }
+        public void DrawLineInt(int degree)
+        {
+            Graphics GPS = this.CreateGraphics();
+            // Create pen.
+            GPS.Clear(this.BackColor);
+
+            // Create coordinates of points that define line.
+            int length = 60;
+            int x1 = 200;
+            int y1 = 175;
+            int x2 = x1 + (int)(length * (Math.Cos(degree * 3.1415926535 / 180)));
+            int y2 = y1 + (int)(length * (Math.Sin(degree * 3.1415926535 / 180)));
+           // label1.Text = degree + "°"+x2+" "+y2+" "+ Math.Cos(degree )+ " "+ Math.Sin(degree);
+            toolStripStatusLabel_Degree_value.Text = degree + "°";
+
+
+            // Draw line to screen.
+            GPS.DrawEllipse(blackPen, x1-length, y1-length, length*2, length*2);
+            GPS.DrawLine(blackPen, x1, y1, x2, y2);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            DrawLineInt((count>=360?count=0:count++));
+
+            
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+            toolStripStatusLabel_timer_interval.Text = trackBar1.Value+" ms ";
+        }
+
+        private void Time_Start_checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if(Time_Start_checkBox.Checked)
+                timer1.Start();
+            else
+                timer1.Stop();
+        }
+    }
+}
